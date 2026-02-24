e86f7a80dd6c46db0dda2d26e2d90c5e80290fcb
Hussa <Hussa@HUSSAM>
2026-02-24 13:40:53 +0300

    Create Business Card Service Interface and implement it

---
+++ LearningTask1/Services/BusinessCardService.cs
@@ -0,0 +1,69 @@
+﻿using LearningTask1.Data;
+using LearningTask1.Dtos;
+using LearningTask1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningTask1.Services
+{
+    public class BusinessCardService(AppDbContext context) : IBusinessCardService
+    {
+
+        public async Task<BusinessCard> AddBusinessCardAsync(CreateBusinessCardDto dto)
+        {
+            var entity = new BusinessCard
+            {
+                Name = dto.Name,
+                Gender = dto.Gender,
+                DOB = dto.DOB,
+                Email = dto.Email,
+                PhoneNumber = dto.PhoneNumber,
+                Address = dto.Address
+            };
+
+            context.BusinessCards.Add(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<bool> DeleteBusinessCardAsync(int id)
+        {
+            var existingBusinessCard = await context.BusinessCards.FindAsync(id);
+            if (existingBusinessCard is null)
+                return false;
+
+            context.BusinessCards.Remove(existingBusinessCard);
+            await context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<BusinessCard?> GetBusinessCardByIdAsync(int id)
+        {
+            var result = await context.BusinessCards.FindAsync(id);
+            return result;
+        }
+
+        public async Task<List<BusinessCard>> GetAllBusinessCardsAsync()
+                    => await context.BusinessCards.ToListAsync();
+
+
+        public async Task<bool> UpdateBusinessCardAsync(int id, UpdateBusinessCardDto dto)
+        {
+            var existingBusinessCard = await context.BusinessCards.FindAsync(id);
+            if (existingBusinessCard is null)
+            {
+                return false;
+            }
+
+            existingBusinessCard.Name = dto.Name;
+            existingBusinessCard.Gender = dto.Gender;
+            existingBusinessCard.DOB = dto.DOB;
+            existingBusinessCard.Email = dto.Email;
+            existingBusinessCard.PhoneNumber = dto.PhoneNumber;
+            existingBusinessCard.Address = dto.Address;
+
+            await context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
---
+++ LearningTask1/Services/IBusinessCardService.cs
@@ -0,0 +1,14 @@
+﻿using LearningTask1.Dtos;
+using LearningTask1.Models;
+
+namespace LearningTask1.Services
+{
+    public interface IBusinessCardService
+    {
+        Task<List<BusinessCard>> GetAllBusinessCardsAsync();
+        Task<BusinessCard?> GetBusinessCardByIdAsync(int id);
+        Task<BusinessCard> AddBusinessCardAsync(CreateBusinessCardDto dto);
+        Task<bool> UpdateBusinessCardAsync(int id, UpdateBusinessCardDto dto);
+        Task<bool> DeleteBusinessCardAsync(int id);
+    }
+}
