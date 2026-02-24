c5434a2d5832575218485045e3c6c09cd7cfd1d1
Hussa <Hussa@HUSSAM>
2026-02-24 13:27:48 +0300

    Create DB

---
+++ LearningTask1/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using LearningTask1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningTask1.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        
+        
+    }
+}
---
+++ LearningTask1/Models/BusinessCard.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearningTask1.Models
+{
+    public class BusinessCard
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public DateOnly DOB { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
